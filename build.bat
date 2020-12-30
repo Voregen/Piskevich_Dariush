a5aac197392f72be64dde8c9f98071b021938397
Voregen <daryusz@mail.ru>
2020-12-30 03:49:48 +0300

    Delete build.bat

--- build.bat
+++
@@ -1,7 +0,0 @@
-md _build
-cd _build
-set CMAKE_PREFIX_PATH="C:/Qt/5.9.9/msvc2015_64"
-set QT_QMAKE_EXECUTABLE="C:/Qt/5.9.9/msvc2015_64/bin/qmake.exe"
-
-cmake .. -DCMAKE_PREFIX_PATH=%CMAKE_PREFIX_PATH%
-cmake --build .
